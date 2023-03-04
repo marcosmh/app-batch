5fbe7c4279b779c1757f894262aea4d6d0e669d2
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2023-03-04 08:20:34 -0600

    app spring batch

---
+++ src/test/java/com/markcode/appbatch/AppBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markcode.appbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
